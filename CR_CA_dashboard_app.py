c8bab3c01a65473edd8b4332c1a101fbc252af19
nidabijapure23 <75780814+nidabijapure23@users.noreply.github.com>
2025-06-26 14:20:40 +0530

    Update CR_CA_dashboard_app.py

--- CR_CA_dashboard_app.py
+++ CR_CA_dashboard_app.py
@@ -49,11 +49,11 @@ tabs = st.tabs([
 ])
 
 # Helper function to display PDF in Streamlit
-# def show_pdf(file_path):
-#     with open(file_path, "rb") as f:
-#         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-#     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="800px" type="application/pdf"></iframe>'
-#     st.markdown(pdf_display, unsafe_allow_html=True)
+def show_pdf(file_path):
+    with open(file_path, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="800px" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
 
 # --- Tab 1: PPT Slide ---
 with tabs[0]:
